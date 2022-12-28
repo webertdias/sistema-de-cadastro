f2964fe5aca492723ba818103bcebda8be74a8d8
webertdias <wdwebert@gmail.com>
2022-12-28 16:33:13 -0300

    teste 4

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("O Paulo fez o commit sem voce ver");
-console.log("Enviando modificações para o Github");
+console.log("Enviando modificações para o Github.");
